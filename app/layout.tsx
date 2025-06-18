5cf83f37061c60f9fe8efa48c8c312e90c5b3e48
largelanguageman <parapet_pigment0j@icloud.com>
2025-06-19 09:27:54 +1000

    fix adobe library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -27,137 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Data Layer Script - MUST be loaded first */}
         <Script
-          id="data-layer-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dl = (function () {
-                var sdl = {
-                  data: {},
-                  pos: 0,
-                  max: 0,
-                  log: function (message, o) {
-                    window.console && console.log("SDL: ", message, o == undefined ? "" : o);
-                  },
-                  exception: function (e) {
-                    sdl.log("Exception", e);
-                  },
-                  get_var: null,
-                  get_property: function (k) {
-                    var v = _satellite.getObjectProperty(sdl.data, k) == undefined ? "" : _satellite.getObjectProperty(sdl.data, k);
-                    return v;
-                  },
-                  clear_vars: function () {
-                    if (window.s) {
-                      for (p in window.s) {
-                        if (p.match(/^(events|products|eVar|prop|list|campaign)/))
-                          window.s.clearVars(p);
-                      }
-                    }
-                  },
-                  tracking_beacon: function (src) {
-                    var b = document.createElement("img");
-                    b.src = src;
-                    b.style.cssText = "width:1px; height:1px; border:0px; display:none";
-                    document.body.appendChild(b);
-                  },
-                  track: function (event, obj) {
-                    if (event) {
-                      _satellite.track(event, obj);
-                    }
-                  },
-                  process: function (d) {
-                    _satellite.logger.log("DataLayer Event Detected: " + d.event, 1);
-                    sdl.log("processing", d);
-                    sdl.data = d;
-                    sdl.track(d.event, d);
-                  },
-                  processing: false,
-                  init: function () {
-                    window.appEventData = window.appEventData || [];
-                    window.MAX_appEventData_LENGTH = 200;
-                    sdl.max = appEventData.length;
-                    window.appEventData.push = function () {
-                      try {
-                        var result = Array.prototype.push.apply(this, arguments);
-                        sdl.max = sdl.max + arguments.length;
-                        if (sdl.processing) {
-                          return result;
-                        }
-                        if (result > 0 && arguments != undefined && arguments.length > 0 && arguments[0].event) {
-                          while (sdl.max > sdl.pos) {
-                            sdl.processing = true;
-                            window.dl.process(appEventData[sdl.pos]);
-                            sdl.pos++;
-                          }
-                          while (window.appEventData.length > MAX_appEventData_LENGTH) {
-                            window.appEventData.shift();
-                            sdl.pos--;
-                            sdl.max--;
-                          }
-                        }
-                        sdl.processing = false;
-                        return result;
-                      } catch (e) {
-                        sdl.processing = false;
-                        sdl.pos++;
-                        sdl.exception(e);
-                      }
-                    };
-                    sdl.get_var = _satellite.getVar;
-                    _satellite.getVar = function (v) {
-                      result = "";
-                      try {
-                        result = sdl.get_var(v);
-                      } catch (e) {
-                        e.message = "Data Element: " + v + "; " + e.message;
-                        sdl.exception(e);
-                      }
-                      return result;
-                    };
-                    while (sdl.max > sdl.pos) {
-                      window.dl.process(appEventData[sdl.pos]);
-                      sdl.pos++;
-                    }
-                    while (window.appEventData.length > MAX_appEventData_LENGTH) {
-                      window.appEventData.shift();
-                      sdl.pos--;
-                      sdl.max--;
-                    }
-                    sdl.log("Initialised");
-                  },
-                };
-                return {
-                  process: sdl.process,
-                  get_property: sdl.get_property,
-                  tracking_beacon: sdl.tracking_beacon,
-                  init: sdl.init,
-                  data: function () {
-                    return sdl.data;
-                  },
-                };
-              })();
-              
-              // Initialize after DTM loads
-              if (typeof _satellite !== 'undefined') {
-                dl.init();
-              } else {
-                // Wait for DTM to load
-                window.addEventListener('satelliteReady', function() {
-                  dl.init();
-                });
-              }
-            `
-          }}
-        />
-
-        {/* Adobe DTM Script - loaded after data layer */}
-        <Script
-          src="https://assets.adobedtm.com/4ef014d90ea4/9205d627617f/launch-edd81ad3dafb-development.min.js"
+          src="https://assets.adobedtm.com/4ef014d90ea4/9205d627617f/launch-798188fd51d6-development.min.js"
           strategy="afterInteractive"
-          async
         />
       </head>
       <body
